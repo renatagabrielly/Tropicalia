eaf8abaa553e29d97ced97e25deacdb1df6aa28a
renatagabrielly <132918391+renatagabrielly@users.noreply.github.com>
2025-03-24 08:40:10 -0300

    .

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let tamanhoAtualFonte = 1;
 
-    const botaoAumentaFonte = document.getElementById('aumentar-fonte');
+    const aumentaTamanhoBotao = document.getElementById('aumentar-fonte');
 
-    botaoAumentaFonte.addEventListener('click' function (){
+    aumentaTamanhoBotao.addEventListener('click' function (){
         tamanhoAtualFonte += 1 ;
 
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
